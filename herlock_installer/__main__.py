d3976208a0c53d16fe89bf44537af5aeef9150ea
SakirBey1 <sakirhack81@gmail.com>
2021-12-01 20:22:07 +0300

    Update __main__.py

--- herlock_installer/__main__.py
+++ herlock_installer/__main__.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     config['TMP_DOWNLOAD_DIRECTORY'] = "./downloads/"
     config['TZ'] = TZ
     config['TZ_NUMBER'] = "1"
-    config['UPSTREAM_REPO_URL'] = "https://github.com/herlockexe/HerlockUserbot"
+    config['UPSTREAM_REPO_URL'] = "https://github.com/SakirBey1/HerlockUserbot"
     config['WARN_LIMIT'] = "3"
     config['WARN_MODE'] = "gmute"
     config['LANGUAGE'] = LANGUAGE
